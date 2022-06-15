8d5685e987d09cd5612cd1d296a0dca00518c4a6
Alexander Persson <alexander@persson.co>
2022-06-15 13:50:07 +0200

    ✨ Adding Package.swift

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AdaEmbedFramework",
+    defaultLocalization: "en",
+    platforms: [
+      .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "AdaEmbedFramework",
+            targets: ["AdaEmbedFramework"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "AdaEmbedFramework",
+            dependencies: [],
+            path: "EmbedFramework",
+            exclude: [
+              "Info.plist",
+              "en.lproj/EmbedFrameworkView.xib",
+              "AdaEmbed.html",
+              "EmbedTest.html",
+              "EmbedView.xib",
+              "OfflineView.xib",
+              "View.xib",
+            ],
+            resources: [
+              .process("Assets.xcassets"),
+              .process("AdaWebHostViewController.storyboard")
+            ]
+        ),
+    ]
+)
+
