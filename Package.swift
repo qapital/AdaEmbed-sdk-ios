eed86558ce5e68a467718192db2a1c7fe54ef93b
Alexander Persson <alexander@persson.co>
2022-06-15 14:18:20 +0200

    ♻️ Making library dynamic

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "AdaEmbedFramework",
+            type: .dynamic,
             targets: ["AdaEmbedFramework"]
         ),
     ],
